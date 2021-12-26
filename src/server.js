e16564e0dd9c42dc4450cd89b121db306bbc92a3
Edyci123 <bicuedici@gmail.com>
2021-12-26 13:05:29 +0200

    first commit

---
+++ src/server.js
@@ -0,0 +1,149 @@
+
+require('dotenv/config')
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const { hash, compare } = require('bcryptjs');
+const cookieParser = require('cookie-parser');
+const { verify } = require('jsonwebtoken');
+/// const { isEmail } = require('validator');
+const { sendAccessToken, sendRefreshToken, createAccessToken, createRefreshToken } = require('./tokens.js')
+const { isAuth } = require('./isAuth.js');
+
+const server = express();
+
+server.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}));
+server.use(express.json());
+server.use(cookieParser());
+server.use(express.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost:27017/frendzDB');
+
+const UserSchema = mongoose.Schema({
+    username: String, 
+    email: String,
+    password: String,
+    refreshtoken: String,
+    contacts: String 
+});
+
+const Users = new mongoose.model('users', UserSchema);
+
+server.post('/register', async (req, res) => {
+    const { username, email, password } = req.body;
+
+    try {
+        const isUser = await Users.exists({username: username});
+
+        if (isUser) throw new Error('This username already exists!');
+
+        const hashedPassword = await hash(password, 10);
+
+        let newUser = new Users({
+            username: username, 
+            email: email, 
+            password: hashedPassword
+        });
+
+        newUser.save();
+        res.send('User Created!');
+    } catch (err) {
+        res.send({
+            error: `${err.message}`
+        });
+    };
+
+});
+
+server.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const isUser = Users.exists({username: username});
+    
+        if (!isUser) throw new Error('User not found!');
+
+        Users.findOne({username: username}, (err, user) => {
+            const isValid = compare(password, user.password);
+            if (!isValid) throw new Error('Wrong Password!');
+
+            const accesstoken = createAccessToken(user._id);
+            const refreshtoken = createRefreshToken(user._id);
+
+            Users.updateOne({username: username}, {refreshtoken: refreshtoken}, (err) => {
+                if (err)
+                    throw new Error(err.message);
+            });
+            
+            sendRefreshToken(res, refreshtoken);
+            sendAccessToken(req, res, accesstoken);
+        });
+    } catch (err) {
+        res.send({
+            error: `${err.message}`
+        });
+    };
+
+});
+
+server.post('/getContacts', async (req, res) => {
+    try {
+        const userID = isAuth(req);
+        
+        if (userID !== null) {
+            res.send({ 
+                message: 'Contacts sent'
+            });
+        }
+    } catch (err) {
+        res.send({
+            error: `${err.message}` 
+        });
+    };
+});
+
+server.post('/logout', (_req, res) => {
+    res.clearCookie('refreshtoken', { path: '/refresh_token' })
+    res.send({
+        message: 'Logged out!'
+    });
+})
+
+server.post('/refresh_token', (req, res) => {
+    const token = req.cookies.refreshtoken;
+
+    if (!token) return res.send({ accesstoken: '' });
+
+    let payload = null;
+    try {
+        payload = verify(token, process.env.REFRESH_TOKEN_SECRET);
+    } catch (err) {
+        res.send({
+            error: `${err.message}`
+        });
+    };
+
+    const isUser = Users.exists({_id: payload.userID});
+    if (!isUser) res.send({ accesstoken: '' });
+
+    const accesstoken = createAccessToken(payload.userID);
+    const refreshtoken = createRefreshToken(payload.userID);
+
+    Users.updateOne({_id: payload.userID}, {refreshtoken: refreshtoken}, (err) => {
+        if (err) {
+            return res.send({ error: `${err.message}` });
+        } else {
+            sendRefreshToken(res, refreshtoken);
+            return res.send({ accesstoken });
+        }
+    });
+
+});
+
+
+server.listen(process.env.PORT);
+
+
---
+++ src/tokens.js
@@ -0,0 +1,34 @@
+const { verify, sign } = require('jsonwebtoken')
+
+const createAccessToken = userID => {
+    return sign({userID}, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '15m'
+    });
+};
+
+const createRefreshToken = userID => {
+    return sign({userID}, process.env.REFRESH_TOKEN_SECRET,  {
+        expiresIn: '15m'
+    });
+};
+
+const sendAccessToken = (req, res, accesstoken) => {
+    res.send({
+        accesstoken,
+        username: req.body.username
+    });
+};
+
+const sendRefreshToken = (res, refreshtoken) => {
+    res.cookie('refreshtoken', refreshtoken, {
+        httpOnly: true,
+        path: '/refresh_token'
+    });
+};
+
+module.exports = {
+    createAccessToken,
+    createRefreshToken,
+    sendAccessToken,
+    sendRefreshToken
+};
